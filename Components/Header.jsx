8f2bee2b4bc1c433e53bbebcacb9b686aea35be8
Robert Finkley <me@robertfinkley.com>
2022-11-04 19:39:58 -0400

    updated login buttong

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import Link from 'next/link';
 import styles from './Header.module.css';
+import { signIn } from 'next-auth/react';
 
 const Header = () => {
-    return (
-      <header className={styles.header}>
-        <div>
-          <Link href="/">
-            <h3>NextAuth Example</h3>
-          </Link>
-        </div>
-        <nav>
-          <ul className={styles.navbar}>
-            <li className={styles.link}>
-              <Link href="/api/auth/login">Login</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    );
+  return (
+    <header className={styles.header}>
+      <div>
+        <Link href="/">
+          <h3>NextAuth Example</h3>
+        </Link>
+      </div>
+      <nav>
+        <ul className={styles.navbar}>
+          <li className={styles.link}>
+            <button onClick={() => signIn()}>Sign In</button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
